88360954df5b7f14c29f86209c0ffac69cdb74a4
prevail90 <90661674+prevail90@users.noreply.github.com>
2025-08-27 13:47:54 +0900

    Create access.php

---
+++ db/access.php
@@ -0,0 +1,8 @@
+<?php
+// Capability definition (none needed for global injection, but for completeness).
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    // No specific capabilities required.
+);
