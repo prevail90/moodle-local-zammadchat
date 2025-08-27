59b3f9b9031b27e89270d8163764ee91ebd12e68
prevail90 <90661674+prevail90@users.noreply.github.com>
2025-08-27 13:46:40 +0900

    Add files via upload

---
+++ lib.php
@@ -0,0 +1,34 @@
+<?php
+// This file hooks into Moodle to inject Zammad chat bubble JS.
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Injects Zammad chat bubble JS on every page.
+ */
+function local_zammadchat_before_footer() {
+    global $PAGE;
+    // Only add on normal pages (not during install, upgrade, etc)
+    if ($PAGE->user_is_editing()) {
+        // Don't inject during editing mode to avoid confusion.
+        return;
+    }
+
+    // Add jQuery (if not already present)
+    $PAGE->requires->js(new moodle_url('https://code.jquery.com/jquery-3.6.0.min.js'));
+
+    // Add Zammad chat script
+    $PAGE->requires->js(new moodle_url('https://support.operatortraining.academy/assets/chat/chat.min.js'));
+
+    // Custom JS to launch chat bubble
+    $js = <<<EOD
+jQuery(function() {
+  new ZammadChat({
+    background: 'rgb(48,67,63)',
+    fontSize: '12px',
+    chatId: 1
+  });
+});
+EOD;
+    $PAGE->requires->js_foot_code($js);
+}
---
+++ version.php
@@ -0,0 +1,10 @@
+<?php
+// This file defines the version and other meta-data for the Moodle plugin.
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_zammadchat'; // Full name of the plugin (used for diagnostics).
+$plugin->version   = 2025082700;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2020061500;         // Requires Moodle 3.9 or later.
+$plugin->maturity  = MATURITY_STABLE;    // This is considered as stable version.
+$plugin->release   = '1.0.0';            // This is our first version.
